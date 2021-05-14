1bed6e083b4ba6c653b5939ac7be814275f890d4
JoelYunga <joel_yunga@hotmail.com>
2021-05-14 15:14:14 -0500

    Funciones Obtener Datos y Ciudad del archivo JSON

--- controlador/estadistica.js
+++ controlador/estadistica.js
@@ -1,19 +1,33 @@
 const fs = require('fs');
 const colors = require("colors");
+const csv = require("csvtojson");
 
-let datosEstadisticos = [];
+async function obtenerDatos(file) {
+    try {
+        const datos = await csv().fromFile(file);
+        let data = [];
+        var codes = JSON.parse(fs.readFileSync('./data/ISO-3166-ALPHA-3.json', 'utf8'));
+        for (let dat of datos) {
+            dat = Object.values(dat);
+            for (let cod of codes) {
+                if (dat[1] == cod.countryCode) {
+                    data.push(dat);
+                }
+            }
+        }
 
-const guardarDatos = () => {
-    let data = JSON.stringify(datosEstadisticos);
-    fs.writeFile('./data/datos.json', data, (err, data) => {
-        if (err) throw new Error('No se pudo guardar la data', err);
-    });
+        return data;
+    } catch (e) {
+        e = "Error 200"
+        return e
+    }
 }
 
-const leerDatos = () => {
-    try {
-        datosEstadisticos = require('../data/datos.json');
-    } catch (error) {
-        datos = []
+async function obtenerPais(pais, codigo) {
+    for (var i = 0; i < pais.length; i++) {
+        let valor = Object.values(pais[i]);
+        if (valor[1] == codigo) {
+            return true
+        }
     }
 }
